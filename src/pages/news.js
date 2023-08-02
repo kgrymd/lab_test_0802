b6de5bdee44c122bb0d8e447505e56a76c78204d
kgrymd <kgroymd@gmail.com>
2023-08-02 15:59:12 +0900

    commit

---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "../styles/test.module.scss"
+
+const news = () => {
+    return (
+        <div className="text-5xl underline">
+            <div className={style.test}>テスト</div>
+        </div>
+    )
+}
+
+export default news
