cce808d0fe0c39b5df91ec28e8515eb0a6cb6708
kgrymd <kgroymd@gmail.com>
2023-08-09 13:53:58 +0900

    use sterwars api

--- src/pages/NewsSlider.js
+++ src/pages/NewsSlider.js
@@ -1,75 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from "../styles/test.module.scss"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const NewsSlider = () => {
-    const news = [
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-        {
-            date: "2021.07.31",
-            title: "30秒の採用動画で「本来出会うべき出会い」を創出する。",
-            description: "G’s発スタートアップ「moovy」の今とこれから",
-            imageUrl: "https://gsacademy.jp/uploads/moovy_photo1-1280x855.jpg"
-        },
-    ];
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch('https://akabab.github.io/starwars-api/api/all.json')
+            .then(response => response.json())
+            .then(data => setCharacters(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3, // 一度に表示するスライドの数を3に変更
+        slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true, // 自動でスライドするように設定
-        autoplaySpeed: 2000, // 2秒ごとにスライド
+        autoplay: true,
+        autoplaySpeed: 2000,
     };
 
     return (
         <>
-            <h3 className={style.title}>INTERVIEW</h3>
-            <p className={style.subtitle}>起業家のインタビュー</p>
+            <h3 className={style.title}>Star Wars Characters</h3>
+            <p className={style.subtitle}>A long time ago in a galaxy far, far away....</p>
             <Slider {...settings}>
-                {news.map((newsItem, index) => (
+                {characters.map((character, index) => (
                     <div key={index} className={style.newsItem}>
-                        <img src={newsItem.imageUrl} alt={newsItem.title} />
-                        <h3>{newsItem.date}</h3>
-                        <h2>{newsItem.title}</h2>
-                        <p>{newsItem.description}</p>
+                        <img src={character.image} alt={character.name} />
+                        <h2>{character.name}</h2>
+                        <p>Gender: {character.gender}</p>
+                        <p>Birth Year: {character.born}</p>
+                        <p>Species: {character.species}</p>
+                        <p>Homeworld: {character.homeworld}</p>
+                        <p>Height: {character.height}m</p>
+                        <p>Mass: {character.mass}kg</p>
+                        <p>Hair Color: {character.hairColor}</p>
+                        <p>Eye Color: {character.eyeColor}</p>
+                        <p>Skin Color: {character.skinColor}</p>
+                        <p>Cybernetics: {character.cybernetics}</p>
+                        <p><a href={character.wiki} target="_blank" rel="noopener noreferrer">Learn more</a></p>
                     </div>
                 ))}
             </Slider>
             <div className={style.button}>
-                <button>記事一覧を見る</button>
+                <button>See more characters</button>
             </div>
         </>
     );
